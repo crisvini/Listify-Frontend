f7669a9fdb82b603428aaf6129c96de5d524bc79
crisvini <crisvini.leoncini@outlook.com>
2023-07-31 11:31:01 -0300

    landing page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import Logo from '../assets/logos/Horizontal.png'
+import LogoSquare from '../assets/logos/ListifySquare.png'
 
 const Home = () => {
     const navigate = useNavigate()
@@ -14,11 +15,61 @@ const Home = () => {
                         </div>
                         <div className="col-12 col-lg-6 text-center text-lg-end mt-2 mt-lg-0">
                             <button type="button" className="btn btn-outline-grey me-2" onClick={() => navigate('/login')}>Login</button>
-                            <button type="button" className="btn btn-orange" onClick={() => navigate('/cadastrar')}>Sign-up</button>
+                            <button type="button" className="btn btn-orange" onClick={() => navigate('/cadastrar')}>Cadastrar</button>
                         </div>
                     </div>
                 </div>
             </header>
+            <main className="container-xxl mt-1 mt-lg-3">
+                <div className="row">
+                    <div className="col-12 col-lg-6 order-lg-1">
+                        <img src={LogoSquare} alt="Listify" className="w-100" />
+                    </div>
+                    <div className="col-12 col-lg-6 align-self-center order-lg-0 mt-1">
+                        <h1>Listas e Anotações Simplificadas.</h1>
+                        <p>Organização descomplicada - Teste o Listify gratuitamente e experimente o poder das listas e anotações simplificadas.</p>
+                        <button type="button" className="btn btn-lg btn-orange w-100-custom">Cadastrar gratuitamente</button>
+                    </div>
+                </div>
+                <div className="row mt-lg-5 g-4 py-5 row-cols-1 row-cols-lg-3">
+                    <div className="col d-flex align-items-start">
+                        <div className="bg-orange p-2 lh-1 rounded-2 d-inline-flex align-items-center justify-content-center fs-4 me-3">
+                            <i className="bi bi-journal-bookmark"></i>
+                        </div>
+                        <div>
+                            <h3 className="fs-2">Organização intuitiva</h3>
+                            <p>Crie tags para categorizar suas listas e anotações de forma mais simples.</p>
+                        </div>
+                    </div>
+                    <div className="col d-flex align-items-start">
+                        <div className="bg-orange p-2 lh-1 rounded-2 d-inline-flex align-items-center justify-content-center fs-4 me-3">
+                            <i className="bi bi-cloud-arrow-up"></i>
+                        </div>
+                        <div>
+                            <h3 className="fs-2">Sincronização em tempo real</h3>
+                            <p>Acesse suas listas e anotações em todos os dispositivos, de onde estiver.</p>
+                        </div>
+                    </div>
+                    <div className="col d-flex align-items-start">
+                        <div className="bg-orange p-2 lh-1 rounded-2 d-inline-flex align-items-center justify-content-center fs-4 me-3">
+                            <i className="bi bi-palette"></i>
+                        </div>
+                        <div>
+                            <h3 className="fs-2">Personalização completa</h3>
+                            <p>Personalize sua anotação como você quiser, com cores, tamanhos e fontes.</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer className="mt-1 mt-lg-3 bg-secondary-custom py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col text-center">
+                            <span>&copy; 2023 Listify. Todos os direitos reservados.</span>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
